3a38c2c7b1c87d4c5d688158eb6efda528e20e13
ufo22940268 <ufo22940268@gmail.com>
2015-04-28 13:21:53 +0800

    Initial Commit

---
+++ ApkInstaller/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ApkInstaller
+//
+//  Created by ccheng on 4/28/15.
+//  Copyright (c) 2015 Frank Cheng. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ApkInstaller/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ApkInstaller
+//
+//  Created by ccheng on 4/28/15.
+//  Copyright (c) 2015 Frank Cheng. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
